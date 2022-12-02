6dd343cb51dcda473f2e564f532a3502fb2f2272
sivajithms <sivajithm@gmail.com>
2022-12-02 16:58:29 +0530

    option to add product and retrieve from database

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,4 +1,5 @@
 var db=require('../config/connection')
+var collection=require('../config/collections')
 
 module.exports={
 
@@ -11,6 +12,12 @@ module.exports={
 
             callback(data.insertedId)
 
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTIONS).find().toArray()
+            resolve(products)
         })
     }
 }
