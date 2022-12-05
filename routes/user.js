c145c943e41635de547b892624e9064964ff20af
sivajithms <sivajithm@gmail.com>
2022-12-05 15:56:37 +0530

    added add to cart and user cart are diplayed from db on cart page

--- routes/user.js
+++ routes/user.js
@@ -14,11 +14,15 @@ const verifyLogin=(req,res,next)=>{
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',async function(req, res, next) {
   let user=req.session.user
   console.log(user)
+  let cartCount=null
+  if(user){
+  cartCount=await userhelpers.getCartCount(req.session.user._id)
+  }
   productHelper.getAllProducts().then((products)=>{
-    res.render('user/view-products', {products,user});
+    res.render('user/view-products', {products,user,cartCount});
   })
 
   
@@ -38,6 +42,9 @@ router.get('/signup',(req,res)=>{
 router.post('/signup',(req,res)=>{
    userhelpers.doSignup(req.body).then((response)=>{
     console.log(response)
+    req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
    })
 })
 
@@ -59,11 +66,17 @@ router.get('/logout',(req,res)=>{
   res.redirect('/')
 })
 
-router.get('/cart',verifyLogin,(req,res)=>{
-  res.render('user/cart')
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userhelpers.getCartProducts(req.session.user._id)
+  console.log(products);
+  res.render('user/cart',{products,user:req.session.user})
 })
 
-
+router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+  userhelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
+    res.redirect('/')
+  })
+})
 
 
 module.exports = router;
